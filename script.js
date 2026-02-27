d5855261d2aa1607e26ba9a1e01666be35bf5198
hattayomar35-collab <hattayomar35@gmail.com>
2026-02-27 22:34:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// ===== تشغيل أنيميشن عند تحميل الصفحة =====
 window.addEventListener("DOMContentLoaded", () => {
-
-  // جميع العناصر المهمة للأنيميشن
   const elements = document.querySelectorAll(
     "header, .main-banner, .overlay-text, section, .card, .video-card, footer"
   );
 
-  // إضافة class مع تأخير بسيط لكل عنصر
   elements.forEach((el, index) => {
     el.style.animationDelay = `${index * 0.1}s`;
     el.classList.add("fade-in");
   });
-
 });
